331181dbcaf4ec6798d6b3a35074011179a6f1de
Marcin Gorzynski <marcin@gorzynski.me>
2021-11-07 17:11:08 +0100

    My Surfaces view with dummy data

---
+++ frontend/src/components/ui/Cards.js
@@ -0,0 +1,93 @@
+import {Box, Button, Flex, Image, SimpleGrid, Stack, useColorModeValue} from "@chakra-ui/react";
+import {useState} from "react";
+
+
+function Card(props) {
+    const {item} = props
+    return (
+        <Box
+            bg={useColorModeValue('white', 'gray.800')}
+            maxW="sm"
+            borderWidth="1px"
+            rounded="lg"
+            shadow="lg"
+            position="relative">
+
+            <Image
+                src={item.imageURL}
+                alt={`Picture of ${item.name}`}
+                roundedTop="lg"
+            />
+
+            <Box p="6">
+                <Flex mt="1" justifyContent="space-between" alignContent="center">
+                    <Box
+                        fontSize="2xl"
+                        fontWeight="semibold"
+                        as="h4"
+                        lineHeight="tight"
+                        isTruncated>
+                        {item.name}
+                    </Box>
+                </Flex>
+
+                <Flex justifyContent="space-between" alignContent="center">
+                    <Box fontSize="2xl" color={useColorModeValue('gray.800', 'white')}>
+                        <Box as="span" color={'gray.600'} fontSize="md">
+                            Location: &nbsp;
+                        </Box>
+                        <Box as="span" fontSize="md">
+                            New York Times Square
+                        </Box>
+                    </Box>
+                </Flex>
+
+                <Flex justifyContent="space-between" alignContent="center">
+                    <Box fontSize="2xl" color={useColorModeValue('gray.800', 'white')}>
+                        <Box as="span" color={'gray.600'} fontSize="md">
+                            Min Bid: &nbsp;
+                        </Box>
+                        <Box as="span" fontSize="md">
+                            {item.minPrice.toFixed(2)} {item.tokenSymbol}
+                        </Box>
+                    </Box>
+                </Flex>
+            </Box>
+        </Box>)
+}
+
+export default function CardList(props) {
+    const {items, pageSize, onLoadPage} = props;
+    const [page, setPage] = useState(1);
+
+    let pSize = 6;
+    if (pageSize !== undefined && pageSize !== 0) {
+        pSize = pageSize;
+    }
+
+    let pageItems = [];
+    for (let i = pSize * (page - 1); i < pSize * page; i++) {
+        if (i >= items.length) {
+            break;
+        }
+
+        pageItems.push(items[i]);
+    }
+
+    return (<Box w="full" alignItems="center" justifyContent="center">
+        <SimpleGrid minChildWidth="300px" spacing={8} m={10}>
+            {pageItems.map((item) =>
+                <Card item={item}/>
+            )}
+        </SimpleGrid>
+        <Flex w="full" alignItems="center" justifyContent="center" mb={10}>
+            {page - 1 > 0 && (<Button m={1} onClick={() => {
+                setPage(page - 1);
+            }}>{page - 1}</Button>)}
+            <Button m={1}>{page}</Button>
+            <Button m={1} onClick={() => {
+                setPage(page + 1);
+            }}>{page + 1}</Button>
+        </Flex>
+    </Box>)
+}
--- frontend/src/pages/MySurfaces.js
+++ frontend/src/pages/MySurfaces.js
@@ -1,9 +1,67 @@
 import LandingLayout from "../components/layouts/LandingLayout";
+import CardList from "../components/ui/Cards";
+import {Button, Flex, Spacer, Text} from "@chakra-ui/react";
+import {BiPlus} from "react-icons/all";
 
 function MySurfaces(props) {
+    const items = [
+        {
+            isNew: true,
+            imageURL: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.pinimg.com%2Foriginals%2F34%2F86%2F6d%2F34866d3ad6c701dc0cfb1c5cffa4c3b4.jpg&f=1&nofb=1",
+            name: "The New York Digital Billboard",
+            tokenSymbol: "DAI",
+            minPrice: 10,
+        },
+        {
+            isNew: true,
+            imageURL: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.pinimg.com%2Foriginals%2F34%2F86%2F6d%2F34866d3ad6c701dc0cfb1c5cffa4c3b4.jpg&f=1&nofb=1",
+            name: "The New York Digital Billboard",
+            tokenSymbol: "DAI",
+            minPrice: 10,
+        },
+
+        {
+            isNew: true,
+            imageURL: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.pinimg.com%2Foriginals%2F34%2F86%2F6d%2F34866d3ad6c701dc0cfb1c5cffa4c3b4.jpg&f=1&nofb=1",
+            name: "The New York Digital Billboard",
+            tokenSymbol: "DAI",
+            minPrice: 10,
+        },
+        {
+            isNew: true,
+            imageURL: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.pinimg.com%2Foriginals%2F34%2F86%2F6d%2F34866d3ad6c701dc0cfb1c5cffa4c3b4.jpg&f=1&nofb=1",
+            name: "The New York Digital Billboard",
+            tokenSymbol: "DAI",
+            minPrice: 10,
+        },
+        {
+            isNew: true,
+            imageURL: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.pinimg.com%2Foriginals%2F34%2F86%2F6d%2F34866d3ad6c701dc0cfb1c5cffa4c3b4.jpg&f=1&nofb=1",
+            name: "The New York Digital Billboard",
+            tokenSymbol: "DAI",
+            minPrice: 10,
+        },
+        {
+            isNew: true,
+            imageURL: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.pinimg.com%2Foriginals%2F34%2F86%2F6d%2F34866d3ad6c701dc0cfb1c5cffa4c3b4.jpg&f=1&nofb=1",
+            name: "The New York Digital Billboard",
+            tokenSymbol: "DAI",
+            minPrice: 10,
+        },
+    ]
+
     return (
         <LandingLayout>
-            My Surfaces
+            <Flex w={'full'} pl={10} pr={10}>
+                <Text fontSize="xl" fontWeight="semibold">
+                    My Surfaces
+                </Text>
+                <Spacer/>
+                <Button>
+                    <BiPlus size={20}/>&nbsp; Register Surface
+                </Button>
+            </Flex>
+            <CardList items={items}/>
         </LandingLayout>
     );
 }
